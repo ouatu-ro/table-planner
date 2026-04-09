6f936aa48221a80ebfbe765f05612ee6b687c04e
ilikehaskell <ouatubogdanioan@gmail.com>
2026-04-09 20:05:55 +0300

    vite upd

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "/table-planner/",
   plugins: [solid()],
+  build: {
+    target: "esnext",
+  },
 });
